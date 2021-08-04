8b1962ff3e16060db480196ff3abf300c8c9e296
Rodrigo Luna <irodrigoro@gmail.com>
2021-08-03 21:26:14 -0700

    Python flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,8 @@
-from flask import Flask, escape, request, render_template,
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template(
-        "index.html", title="Home"
-    )
 
-@app.route('/flixlist')
+@app.route("/flixlist")
 def flixlist():
-    return render_template(
-        "flixlist.html", title="FlixList"
-    )
+    return render_template("flixlist.html", title="FlixList")
