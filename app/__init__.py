b6b8912f22fe534354f2dd90ab1630695e5bc7a8
AYANA!!!! <59160298+nanonite9@users.noreply.github.com>
2021-08-04 21:44:51 -0400

    Update __init__.py
    Add routes for friends and recs

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,13 @@ app = Flask(__name__)
 def flixlist():
     rows = 10
     return render_template("flixlist.html", title="FlixList", rows=rows)
+
+@app.route("/friendflix")
+def friendflix():
+    rows = 3
+    return render_template("friendflix.html", title="FriendFlix", rows=rows)
+
+@app.route("/recsflix")
+def recsflix():
+    rows = 3
+    return render_template("recsflix.html", title="RecsFlix", rows=rows)
