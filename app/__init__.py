6d4f20c3e3cd9145bcaaec6d4ccf1e866b06f2e3
Rodrigo Luna <irodrigoro@gmail.com>
2021-08-05 17:51:12 -0700

    Add show details template

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,11 @@ app = Flask(__name__)
 @app.route("/flixlist/<menu_item>")
 def flixlist(menu_item):
     rows = 10
-    return render_template("flixlist.html", title="FlixList", menu_item=menu_item, rows=rows)
+    return render_template(
+        "flixlist.html", title="FlixList", menu_item=menu_item, rows=rows
+    )
+
+
+@app.route("/shows/<show_id>")
+def show_details(show_id):
+    return render_template("show_details.html", title=show_id, show_id=show_id)
